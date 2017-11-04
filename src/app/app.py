12ec1ee6d893719b121bb9968b40518a9b345821
Xuanyu Dong <xuanyudo@buffalo.edu>
2017-11-04 17:46:09 -0400

    landing page done

--- src/app/app.py
+++ src/app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/',methods=['GET'])
 @app.route('/index.html',methods=['GET'])
 def index():
-    return return_template('index.html')
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
