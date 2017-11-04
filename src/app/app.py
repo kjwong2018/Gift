d53c001f8f26060e46bd7f8e030cbf7479efc285
kjwong2018 <kjwong@buffaolo.edu>
2017-11-04 10:42:10 -0400

    Added index rendering app,py

--- src/app/app.py
+++ src/app/app.py
@@ -1,1 +1,17 @@
 from flask import Flask,request,jsonify,render_template
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET'])
+@app.route('/index.html',methods=['GET'])
+def index():
+    return return_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
+
+
+# @app.route('/',methods=['GET'])
+# @app.route('/',methods=['GET'])
+# @app.route('/',methods=['GET'])
+# @app.route('/',methods=['GET'])
