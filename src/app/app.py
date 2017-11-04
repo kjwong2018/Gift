ebd20f3702d122bb82954d1cfdffb80e763cd2c3
kjwong2018 <kjwong@buffaolo.edu>
2017-11-04 13:49:11 -0400

    I don't know

--- src/app/app.py
+++ src/app/app.py
@@ -1,11 +1,22 @@
 from flask import Flask,request,jsonify,render_template
+from flask_sqlalchemy import  SQLAlchemy
+# host = "127.0.0.1"#host address
+# port = 8080
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'gift:gift12345@mysql+pymysql://giftdatabase.c03akep9s2fc.us-west-2.rds.amazonaws.com:3306/giftdatabase'
+db = SQLAlchemy(app)
+
+# class data
 
 @app.route('/',methods=['GET'])
 @app.route('/index.html',methods=['GET'])
 def index():
-    return return_template('index.html')
+    return render_template('index.html')
+
+@app.route('/Gift',methods=['GET','POST'])
+def main():
+    return render_template('gift.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
