6337daca4be29c29451570d37cd767ca9b0e8941
kjwong2018 <kjwong@buffaolo.edu>
2017-11-04 20:06:58 -0400

    Progress for database dynamic page

--- src/app/app.py
+++ src/app/app.py
@@ -6,60 +6,56 @@ from sqlalchemy import create_engine
 
 app = Flask(__name__)
 
-# class data
-# def init_engine():
-#     engine = create_engine('gift:gift12345@mysql+pymysql://giftdatabase.c03akep9s2fc.us-west-2.rds.amazonaws.com:3306/giftdatabase')
-
 def get_init():
     myDict = {"init":{}}
     engine = create_engine('mysql+pymysql://gift:gift12345@giftdatabase.c03akep9s2fc.us-west-2.rds.amazonaws.com:3306/gift_db')
     connection = engine.connect()
-    result = connection.execute('select * from tagType;')
-    x = 0
+    result = connection.execute("SELECT tagName	FROM tagType AS t1 INNER JOIN tagTypeMap AS map	ON t1.tagType='Occasion' AND t1.typeID=map.tagTypeID INNER JOIN tag ON tag.tagID=map.tagID;")
+    x = 1
     for i in result:
-        if(i[0] == 1):
-            myDict["init"][i[1]]={}
-            t = i[1]
-        else:
-            myDict["init"][t][x]=i[1]
-            x = x+1
-    print(myDict)
+        myDict["init"][x]=i[0]
+        x +=1
     connection.close()
     return myDict
 
 def get_question():
-    myDict = {"question":{}}
     engine = create_engine('mysql+pymysql://gift:gift12345@giftdatabase.c03akep9s2fc.us-west-2.rds.amazonaws.com:3306/gift_questionair')
     connection = engine.connect()
     result = connection.execute('select * from questionair;')
+    x = 1
+    msg = {"question":{}}
     for i in result:
-        print(i)
+        word_list = i[2].split(",")
+        msg["question"][x] = {"Question":i[1],"Answer":{}}
+        y = 0
+        while len(word_list) != 0:
+            word = word_list.pop(0)
+            msg["question"][x]["Answer"][y]= word
+            y = y+1
+        x = x+1
+    return msg
 
 
 @app.route('/',methods=['GET'])
 @app.route('/index.html',methods=['GET'])
 def index():
-    get_question()
     return render_template('index.html')
 
 @app.route('/pick_gift.html',methods=['GET','POST'])
 def pickGift():
     return render_template('pick_gift.html')
-@app.route('/init',methods=['POST'])
+
+@app.route('/request',methods=['POST'])
 def message():
     rcvd = request.data
-    print(rcvd)
-    # if(rcvd == "init"):
-    msg = get_init()
-    print(msg)
-    return jsonify(msg)
+    if(rcvd == "init"):
+        msg = get_init()
+        print(msg)
+        return jsonify(msg)
+    elif(rcvd == "question"):
+        msg = get_question
+        print(msg)
+        return jsonify(msg)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
-    # init_engine()
-
-
-# @app.route('/',methods=['GET'])
-# @app.route('/',methods=['GET'])
-# @app.route('/',methods=['GET'])
-# @app.route('/',methods=['GET'])
